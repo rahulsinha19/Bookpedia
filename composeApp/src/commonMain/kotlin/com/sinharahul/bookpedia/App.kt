ba2b4617ba18601c0151b82dd7a8823554aeaee6
Rahul Sinha <rahul@rapido.bike>
2025-04-12 15:16:17 +0530

    setting up of ktor http client for network calls

--- composeApp/src/commonMain/kotlin/com/sinharahul/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/sinharahul/bookpedia/App.kt
@@ -6,16 +6,20 @@ import com.sinharahul.bookpedia.book.data.network.KtorRemoteBookDataSource
 import com.sinharahul.bookpedia.book.data.repository.DefaultBookRepository
 import com.sinharahul.bookpedia.book.presentation.book_list.BookListScreen
 import com.sinharahul.bookpedia.book.presentation.book_list.BookListViewModel
+import com.sinharahul.bookpedia.core.data.HttpClientFactory
+import io.ktor.client.engine.HttpClientEngine
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 // 2:39:40
 @Composable
 @Preview
-fun App() {
+fun App(engine: HttpClientEngine) {
     BookListScreen(
         viewModel = remember { BookListViewModel(
             bookRepository = DefaultBookRepository(
-                remoteBookDataSource = KtorRemoteBookDataSource
+                remoteBookDataSource = KtorRemoteBookDataSource(
+                    httpClient = HttpClientFactory.create(engine = engine)
+                )
             )
         ) },
         onBookClick = {}
